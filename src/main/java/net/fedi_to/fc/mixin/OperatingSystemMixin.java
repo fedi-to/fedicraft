5218da8a6bb4c435e40585e57d34a82a31141c31
SoniEx2 <endermoneymod@gmail.com>
2023-08-15 20:31:10 -0300

    Fix mojang

--- src/main/java/net/fedi_to/fc/mixin/OperatingSystemMixin.java
+++ src/main/java/net/fedi_to/fc/mixin/OperatingSystemMixin.java
@@ -5,15 +5,27 @@ import net.fedi_to.fc.ffi.FcDll;
 import net.fedi_to.fc.os.NonWinMac;
 import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Locale;
 
 @Mixin(Util.OperatingSystem.class)
 public class OperatingSystemMixin {
+    @Overwrite
+    public void open(String uri) {
+        Util.OperatingSystem os = (Util.OperatingSystem) (Object) this;
+        try {
+            os.open(new URI(uri));
+        } catch (URISyntaxException | IllegalArgumentException var3) {
+            Fedicraft.LOGGER.error("Couldn't open uri '{}'", uri, var3);
+        }
+    }
     @Inject(method = "open(Ljava/net/URI;)V", at = @At("HEAD"), cancellable = true)
     void fedicraft_openUri(URI uri, CallbackInfo cb) {
         Util.OperatingSystem os = (Util.OperatingSystem) (Object) this;
